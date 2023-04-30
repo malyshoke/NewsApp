fd02c2c4515d7084cbfbaf0bce52e54e36b22cf1
malyshoke <malyshok.ekaterina@yandex.ru>
2023-05-01 00:30:44 +0300

    convert json, DI

---
+++ app/src/main/java/com/malyshok/newsapp/data/api/NewsService.kt
@@ -0,0 +1,21 @@
+package com.malyshok.newsapp.data.api
+
+import com.malyshok.newsapp.utils.Constants.Companion.API_KEY
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsService {
+
+    @GET("/v2/everything")
+    suspend fun getEverything(
+        @Query("q") query: String,
+        @Query("page") page: Int = 1,
+        @Query("apiKey") apiKey: String = API_KEY
+    )
+
+    suspend fun getHeadlines(
+        @Query("country") countryCode: String = "ru",
+        @Query("page") page: Int = 1,
+        @Query("apiKey") apiKey: String = API_KEY
+    )
+}
---
+++ app/src/main/java/com/malyshok/newsapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.malyshok.newsapp.utils
+
+class Constants {
+    companion object{
+        const val BASE_URL = "https://newsapi.org/"
+        const val API_KEY = "b7c42c3ec5b04962b8cacb144c5741d1"
+    }
+}
